899d514cac9575b050861302688471f7f1123b65
youssefreda4 <154443732+youssefreda4@users.noreply.github.com>
2025-03-17 20:56:34 +0200

    Enhancing updating modal

---
+++ resources/views/components/update-modal.blade.php
@@ -0,0 +1,31 @@
+<div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
+    <div class="modal-dialog modal-lg " role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">{{ $title }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <form wire:submit='update'>
+                <div class="modal-body">
+                    <div class="row">
+                        {{ $slot }}
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                        @include('dashboard.loading',[
+                        'target' => 'update',
+                        'name' => 'Update'
+                        ])
+                    </button>
+                </div>
+            </form>
+
+
+        </div>
+    </div>
+</div>
--- resources/views/dashboard/pages/skills/update-skill-component.blade.php
+++ resources/views/dashboard/pages/skills/update-skill-component.blade.php
@@ -1,40 +1,12 @@
-<div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
-    <div class="modal-dialog modal-lg " role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Edit Skill</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <form wire:submit='update'>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-md-6 mb-2">
-                            <label class="form-label">Name</label>
-                            <input type="text" class="form-control" placeholder="Enter Name" wire:model='name' />
-                            <x-input-error :messages="$errors->get('name')" />
-                        </div>
-                        <div class="col-md-6 mb-2">
-                            <label class="form-label">Progress</label>
-                            <input type="number" class="form-control" placeholder="1 - 100" wire:model='progress' />
-                            <x-input-error :messages="$errors->get('progress')" />
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="submit" class="btn btn-primary">
-                        @include('dashboard.loading',[
-                        'target' => 'update',
-                        'name' => 'Update'
-                        ])
-                    </button>
-                </div>
-            </form>
-
-
-        </div>
+<x-update-modal title="Edit Skill">
+    <div class="col-md-6 mb-2">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" placeholder="Enter Name" wire:model='name' />
+        <x-input-error :messages="$errors->get('name')" />
     </div>
-</div>
+    <div class="col-md-6 mb-2">
+        <label class="form-label">Progress</label>
+        <input type="number" class="form-control" placeholder="1 - 100" wire:model='progress' />
+        <x-input-error :messages="$errors->get('progress')" />
+    </div>
+</x-update-modal>
