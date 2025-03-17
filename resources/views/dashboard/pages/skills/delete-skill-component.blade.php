d4e36a58ba8bc74ff1711daf832d27e1bbcd831c
youssefreda4 <154443732+youssefreda4@users.noreply.github.com>
2025-03-17 21:07:19 +0200

    Enhancing deletion modal

--- resources/views/dashboard/pages/skills/delete-skill-component.blade.php
+++ resources/views/dashboard/pages/skills/delete-skill-component.blade.php
@@ -1,31 +1,3 @@
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
-    <div class="modal-dialog modal-lg " role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Delete Skill</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <form wire:submit='delete'>
-                <div class="modal-body">
-                    <strong>
-                        Are you sure want to delete this record ?
-                    </strong>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="submit" class="btn btn-danger">
-                        @include('dashboard.loading',[
-                        'target' => 'delete',
-                        'name' => 'Delete'
-                        ])
-                    </button>
-                </div>
-            </form>
-
-
-        </div>
-    </div>
-</div>
+<x-delete-modal title="Delete Skill">
+    Are you sure want to delete this record ?
+</x-delete-modal>
