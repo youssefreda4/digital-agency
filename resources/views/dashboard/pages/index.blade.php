a0555dedca878dac5b67bd60b682ad00a09ffd87
youssefreda4 <154443732+youssefreda4@users.noreply.github.com>
2025-03-20 06:22:57 +0200

    add newsletters card in home dashboard

--- resources/views/dashboard/pages/index.blade.php
+++ resources/views/dashboard/pages/index.blade.php
@@ -125,6 +125,22 @@
                 </div>
             </div>
         </div>
+        <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
+            <div class="row">
+                 <div class="col-lg-6 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <a href="{{ route('dashboard.newsletters') }}" class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <i class='menu-icon bx bx-envelope'></i>
+                                </div>
+                            </div>
+                            <span class="fw-semibold d-block mb-1">Newsletters</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
